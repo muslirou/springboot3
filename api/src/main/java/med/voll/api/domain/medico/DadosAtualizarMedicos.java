1a4f4652a1a7ecc8a002bb8499b3fad594748b8e
Murilo Domingues <138342590+muslirou@users.noreply.github.com>
2024-07-06 17:21:28 -0300

    Add files via upload

---
+++ api/src/main/java/med/voll/api/domain/medico/DadosAtualizarMedicos.java
@@ -0,0 +1,17 @@
+package med.voll.api.domain.medico;
+
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.endereco.DadosEndereco;
+
+public record DadosAtualizarMedicos(
+
+        @NotNull
+        Long id,
+
+        String nome,
+
+        String telefone,
+
+        DadosEndereco endereco) {
+
+}
